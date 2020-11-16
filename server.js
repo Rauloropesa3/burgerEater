758d8f8e577e2ae1aba02247224f0a9f384b072a
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-11-16 05:53:48 -0800

    updated files

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const exphs = require("express-handlebars");
-
-const app = express();
 
 const PORT = process.env.PORT || 7000;
 
+const app = express();
+
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+const exphs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 //Routes
 const routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
 
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
