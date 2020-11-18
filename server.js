ce2a8e32b0a80ec03260b73f1adb1a4a718bfb28
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-11-17 22:00:33 -0800

    updating

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-const exphs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
