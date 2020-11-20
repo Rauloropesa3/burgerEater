585472e684870ee4ed5e11a24a244db76f7aa878
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-11-19 20:08:48 -0800

    updated schema

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const routes= require("./controller/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function(){
-    console.log("Server listening on: http://localhost" + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
 
