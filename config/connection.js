cabfc686c64df2d9d38ec2eb910321057c67e8e2
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-11-29 04:09:24 -0800

    updated connection to Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
 // configuration connection
-//require("dotenv").config();
+require("dotenv").config();
+
 // requiring my db
 var mysql = require("mysql2");
 
 //JAWS DB set up
 var connection;
-// connection = mysql.createConnection(process.env.JAWSDB_URL);
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "*Itzael2019*",
-  database: "burgers_db",
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "*Itzael2019*",
+    database: "burgers_db",
+  });
+}
+
 // connection
 connection.connect((err) => {
   if (err) {
